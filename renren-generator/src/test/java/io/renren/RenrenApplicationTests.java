4be576ad789a67bf3542219b373aef5fb1b6f171
Mkydy <2062771709@qq.com>
2022-05-18 22:02:23 +0800

    [NO]: FR202205141229 [Describe]: renren-generator 逆向工程 [Issues]: NO

---
+++ renren-generator/src/test/java/io/renren/RenrenApplicationTests.java
@@ -0,0 +1,14 @@
+package io.renren;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RenrenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
